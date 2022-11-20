3db4c5e829e18a8894c605cc9411ecc1199dbfdf
Asha Bhat <43027008+ashabhats@users.noreply.github.com>
2022-11-20 17:30:22 +0530

    Added multielect

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,13 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title(' 🍌🍊🥭🍓🍒 Build your own Fruit Smoothie🍑🍏🍐🍎 ' )
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+
+
 streamlit.title('💖 I Love Eeshan & Pranav 💖' )
 
 streamlit.header('Both are very good boys! 👌')
