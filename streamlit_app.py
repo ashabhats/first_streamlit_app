4ea7579b4798bf7b0c5c9cd79b9f1d384f50b357
Asha Bhat <43027008+ashabhats@users.noreply.github.com>
2022-11-20 17:23:21 +0530

    Fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('💖 I Love Eeshan & Pranav'💖 )
+streamlit.title('💖 I Love Eeshan & Pranav 💖' )
 
 streamlit.header('Both are very good boys! 👌')
 streamlit.text('Both are bigtime foodies!! 😊')
